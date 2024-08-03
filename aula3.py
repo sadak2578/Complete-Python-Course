cdd20aa635e60b8e35eaa8239e9a378a1f58ced3
sadak2578 <108498678+sadak2578@users.noreply.github.com>
2024-08-03 14:59:54 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,10 @@
+'''
+Python é uma linguagem de programação com o tipo de tipagem Dinâmica/Forte
+o que signfica tipagem dinâmica? significa que o python já sabe o tipo de informação que estmamos passando para ele
+já a tipagem forte significa que o tipo de variável ele é definido  e não muda automaticamente
+'''
+# números
+print(1234) # -> tipagem dinâmica, o Python já reconhece o tipo de dado simples que estamos utilizando aqui
+
+#strings
+print('Olá mundo, python!')
---
+++ aula4.py
@@ -0,0 +1,19 @@
+# ========== Tipos primitivos de dados ==========
+
+# o primeiro tipo de dado primitivo é o tipo inteiro
+
+print(12,-12,0,45, sep='|') #este é um tipo de dado primitivo inteiro
+
+
+
+
+# Float -> é um tipo de número com ponto flutuante
+# ou seja, é um tipo de dado com casas decimais
+print(3.4, 2.45, 20.9823464, 3.1415,sep='|') #float
+print(0.0001, -15.786, sep='|') #float
+
+#Função que mostra o tipo de dado em python nós chamamos de Type
+print(type('Olá mundo'))
+print(type(-23))
+print(type(0))
+print(type(12.9), type(134.23), type(0.12387267), sep='|')
