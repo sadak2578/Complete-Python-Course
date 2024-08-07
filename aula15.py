d24233de12c7fcda05c2bc862c0a63a981840bdc
sadak2578 <108498678+sadak2578@users.noreply.github.com>
2024-08-07 11:54:16 -0300

    Create aula15.py

---
+++ aula15.py
@@ -0,0 +1,12 @@
+#nome = input('qual seu nome?: ')
+
+#print(f'seu nome é {nome = }') #isso mostra o valor da variável
+#print(f'seu nome é: {nome}') 
+numero1 = input('Digite o primeiro número: ')
+
+numero2 = input('Digite o segundo número: ')
+int_numero1 = int(numero1)
+int_numero2 = int(numero2)
+soma = int_numero1 + int_numero2
+
+print(f'A soma dos números é: {soma}')
